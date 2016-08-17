6ba01c3947b8d3107147b5978d3eba693188255c
Vijay Bhargav Kongari <vbkongari6@outlook.com>
2016-08-16 18:07:46 -0700

    feat(module-api): edit WebConfig file to authorize access data from other ports

--- server/dramaflix/src/main/java/com/vbkongari/dramaflix/WebConfig.java
+++ server/dramaflix/src/main/java/com/vbkongari/dramaflix/WebConfig.java
@@ -2,11 +2,22 @@ package com.vbkongari.dramaflix;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+    	registry.addMapping("/**/*")
+				.allowedOrigins("*")
+				.allowedHeaders("Authorization", "Origin", "Content-Type")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowCredentials(false)
+				.maxAge(3600);
+    }
 }
